76029e3032b0aea5351c41c4ea6418a9cd5c29a0
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2023-01-29 10:09:02 +0545

    moved to root

--- App.js
+++ App.js
@@ -2,9 +2,12 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  console.log("Hi");
   return (
+
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+
+      <Text>I'm working on my app!</Text>
       <StatusBar style="auto" />
     </View>
   );
