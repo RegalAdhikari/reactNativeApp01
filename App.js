fa975bf097d80dc85eb8052df29a6203df44b6d0
Regal Adhikari <65963700+RegalAdhikari@users.noreply.github.com>
2023-01-30 23:12:04 +0545

    bundling .apk

--- App.js
+++ App.js
@@ -1,19 +1,81 @@
-import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, SafeAreaView, Image } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  SafeAreaView,
+  Image,
+  TouchableNativeFeedback,
+  Button,
+  Alert,
+  Platform,
+  StatusBar,
+} from "react-native";
 
 export default function App() {
+  const [count, setCount] = useState(1);
   return (
     <SafeAreaView style={styles.container}>
-      <Text>I'm working on my app!</Text>
-      <Text>Hey</Text>
+      <Text style={{ fontSize: 30 }}>My first React Native App.</Text>
+      <Text style={{ fontSize: 18 }}>JavaScript is a very cool language.</Text>
+      <TouchableNativeFeedback onPress={() => setCount(count + 1)}>
+        <View
+          style={{
+            width: 200,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: "pink",
+            margin: 20,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "blue",
+            }}
+          >
+            Press me
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
       <Image
-        source={{
-          width: 200,
-          height: 400,
-          uri: "https://picsum.photos/200/400",
-        }}
+        style={{ marginBottom: 20 }}
+        source={require("./assets/react.png")}
       />
-      {/* <StatusBar style="auto" /> */}
+      <Text style={{ fontSize: 18 }}>
+        I'm imPressed {count} times by JavaScript.
+      </Text>
+      <TouchableNativeFeedback onPress={() => setCount(count - 1)}>
+        <View
+          style={{
+            width: 200,
+            height: 50,
+            borderRadius: 25,
+            backgroundColor: "violet",
+            margin: 20,
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Text
+            style={{
+              color: "red",
+            }}
+          >
+            I hate JavaScript
+          </Text>
+        </View>
+      </TouchableNativeFeedback>
+      <Button
+        title="Push Me"
+        onPress={() =>
+          Alert.alert("Do YOU love JavaScript?", "Please say Yes", [
+            { text: "Yes" },
+            { text: "Absolutely Yes" },
+          ])
+        }
+      ></Button>
     </SafeAreaView>
   );
 }
@@ -24,6 +86,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#ff6f61",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0,
   },
   left: {
     alignItems: "left",
